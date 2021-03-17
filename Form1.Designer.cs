a36e058e073b3e95bdddb3aff63cd4efacb9c401
Cody Uhlrich <smartturkeycj@gmail.com>
2021-03-16 20:30:41 -0500

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Chaos_Mover
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.sdGTA = new System.Windows.Forms.SaveFileDialog();
+            this.tvGTA = new System.Windows.Forms.TreeView();
+            this.tvStorage = new System.Windows.Forms.TreeView();
+            this.btnUninstall = new System.Windows.Forms.Button();
+            this.btnInstall = new System.Windows.Forms.Button();
+            this.btnPickPath = new System.Windows.Forms.Button();
+            this.txtInstallPath = new System.Windows.Forms.TextBox();
+            this.odGTA = new System.Windows.Forms.OpenFileDialog();
+            this.fdGTA = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sdGTA
+            // 
+            this.sdGTA.FileOk += new System.ComponentModel.CancelEventHandler(this.sdGTA_FileOk);
+            // 
+            // tvGTA
+            // 
+            this.tvGTA.Location = new System.Drawing.Point(9, 42);
+            this.tvGTA.Name = "tvGTA";
+            this.tvGTA.Size = new System.Drawing.Size(194, 97);
+            this.tvGTA.TabIndex = 0;
+            // 
+            // tvStorage
+            // 
+            this.tvStorage.Location = new System.Drawing.Point(290, 42);
+            this.tvStorage.Name = "tvStorage";
+            this.tvStorage.Size = new System.Drawing.Size(194, 97);
+            this.tvStorage.TabIndex = 1;
+            // 
+            // btnUninstall
+            // 
+            this.btnUninstall.Location = new System.Drawing.Point(209, 42);
+            this.btnUninstall.Name = "btnUninstall";
+            this.btnUninstall.Size = new System.Drawing.Size(75, 23);
+            this.btnUninstall.TabIndex = 2;
+            this.btnUninstall.Text = "-->";
+            this.btnUninstall.UseVisualStyleBackColor = true;
+            this.btnUninstall.Click += new System.EventHandler(this.btnUninstall_Click);
+            // 
+            // btnInstall
+            // 
+            this.btnInstall.Location = new System.Drawing.Point(209, 116);
+            this.btnInstall.Name = "btnInstall";
+            this.btnInstall.Size = new System.Drawing.Size(75, 23);
+            this.btnInstall.TabIndex = 3;
+            this.btnInstall.Text = "<--";
+            this.btnInstall.UseVisualStyleBackColor = true;
+            this.btnInstall.Click += new System.EventHandler(this.btnInstall_Click);
+            // 
+            // btnPickPath
+            // 
+            this.btnPickPath.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPickPath.BackgroundImage")));
+            this.btnPickPath.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPickPath.Location = new System.Drawing.Point(9, 6);
+            this.btnPickPath.Name = "btnPickPath";
+            this.btnPickPath.Size = new System.Drawing.Size(30, 30);
+            this.btnPickPath.TabIndex = 4;
+            this.btnPickPath.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnPickPath.UseVisualStyleBackColor = true;
+            this.btnPickPath.Click += new System.EventHandler(this.btnPickPath_Click);
+            // 
+            // txtInstallPath
+            // 
+            this.txtInstallPath.Location = new System.Drawing.Point(45, 12);
+            this.txtInstallPath.Name = "txtInstallPath";
+            this.txtInstallPath.Size = new System.Drawing.Size(158, 20);
+            this.txtInstallPath.TabIndex = 5;
+            // 
+            // odGTA
+            // 
+            this.odGTA.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 31);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Mod Status:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(171, 142);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(116, 31);
+            this.lblStatus.TabIndex = 7;
+            this.lblStatus.Text = "Installed";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 180);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInstallPath);
+            this.Controls.Add(this.btnPickPath);
+            this.Controls.Add(this.btnInstall);
+            this.Controls.Add(this.btnUninstall);
+            this.Controls.Add(this.tvStorage);
+            this.Controls.Add(this.tvGTA);
+            this.Name = "frmMain";
+            this.Text = "GTA File Mover";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog sdGTA;
+        private System.Windows.Forms.TreeView tvGTA;
+        private System.Windows.Forms.TreeView tvStorage;
+        private System.Windows.Forms.Button btnUninstall;
+        private System.Windows.Forms.Button btnInstall;
+        private System.Windows.Forms.Button btnPickPath;
+        private System.Windows.Forms.TextBox txtInstallPath;
+        private System.Windows.Forms.OpenFileDialog odGTA;
+        private System.Windows.Forms.FolderBrowserDialog fdGTA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chaos_Mover
+{
+    public partial class frmMain : Form
+    {
+        private string stashPath = "";
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            var configKeys = new List<string>
+            {
+                "GTA5_Install_Path"
+            };
+
+            foreach (var myKey in configKeys) {
+                switch (myKey) {
+                    case "GTA5_Install_Path":
+                        txtInstallPath.Text = GetConfigSetting(myKey);
+                        fdGTA.SelectedPath = txtInstallPath.Text;
+                        break;
+                }
+            }
+
+            var location = new Uri(Assembly.GetEntryAssembly().GetName().CodeBase);
+            stashPath = new FileInfo(location.AbsolutePath).Directory.FullName + "\\Stashed";
+
+            if (!Directory.Exists(stashPath))
+            {
+                Directory.CreateDirectory(stashPath);
+            }
+
+            UpdateStatus();
+        }
+
+        private string GetConfigSetting(string key)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = config.AppSettings.Settings;
+            var value = settings[key]?.Value;
+
+            if (string.IsNullOrEmpty(value))
+            {
+                value = "";
+                settings.Add(key, value);
+            }
+
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(config.AppSettings.SectionInformation.Name);
+            return value;
+        }
+
+        private void SaveConfigSetting(string key, string value)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = config.AppSettings.Settings;
+
+            settings[key].Value = value;
+
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(config.AppSettings.SectionInformation.Name);
+        }
+
+
+        private void sdGTA_FileOk(object sender, CancelEventArgs e)
+        {
+            txtInstallPath.Text = sdGTA.FileName;
+            SaveConfigSetting("GTA5_Install_Path", sdGTA.FileName);
+        }
+
+        private void btnPickPath_Click(object sender, EventArgs e)
+        {
+            var currentValue = txtInstallPath.Text.Trim();
+
+            if (Directory.Exists(currentValue))
+            {
+                fdGTA.SelectedPath = currentValue;
+            }
+
+            PickInstallPath();
+        }
+
+        private bool PickInstallPath()
+        {
+            DialogResult result = fdGTA.ShowDialog();
+            var selectedPath = fdGTA.SelectedPath;
+
+            if (result == DialogResult.OK)
+            {
+                selectedPath = fdGTA.SelectedPath;
+                txtInstallPath.Text = selectedPath;
+                SaveConfigSetting("GTA5_Install_Path", selectedPath);
+            }
+
+            return HaveValidPath();
+        }
+
+        private bool HaveValidPath()
+        {
+            return Directory.Exists(fdGTA.SelectedPath);
+        }
+
+
+        private List<string> GetAllModFiles(string fromPath)
+        {
+            var asiFiles = Directory.GetFiles(fromPath, "*.asi");
+            var dllFiles = Directory.GetFiles(fromPath, "*.dll");
+            var modPath = $"{fromPath}\\chaosmod";
+            var chaosMod = Directory.Exists(modPath);
+            var modFiles = new List<string>();
+
+            TreeView saveTree = tvStorage;
+
+            if (fromPath == fdGTA.SelectedPath)
+            {
+                saveTree = tvGTA;
+            }
+
+            saveTree.Nodes.Clear();
+
+            if (asiFiles.Length + dllFiles.Length > 0 && chaosMod)
+            {
+                var modDlls = new List<string>()
+                {
+                    "dinput8.dll",
+                    "ScriptHookV.dll"
+                };
+
+                modFiles.Add(modPath);
+                modFiles.AddRange(asiFiles.ToList<string>());
+
+                foreach (string item in dllFiles)
+                {
+                    var file = item.Replace(fromPath + "\\", "");
+
+                    if (modDlls.Contains(file))
+                    {
+                        modFiles.Add(item);
+                    }
+                }
+
+                foreach (string item in modFiles)
+                {
+                    saveTree.Nodes.Add(item.Replace(fromPath + "\\", ""));
+                }
+            };
+
+            return modFiles;
+        }
+
+        private void btnInstall_Click(object sender, EventArgs e)
+        {
+            var modFiles = GetAllModFiles(stashPath);
+            MoveFiles(stashPath, fdGTA.SelectedPath, modFiles);
+        }
+
+        private void btnUninstall_Click(object sender, EventArgs e)
+        {
+            var modFiles = GetAllModFiles(fdGTA.SelectedPath);
+            MoveFiles(fdGTA.SelectedPath, stashPath, modFiles);
+        }
+
+        private void MoveFiles(string fromPath, string toPath, List<string> modFiles)
+        {
+            foreach (string item in modFiles)
+            {
+                var file = item.Replace($"{fromPath}\\", "");
+                var test = $"{toPath}\\{file}";
+                var oops = "";
+                Directory.Move(item, $"{toPath}\\{file}");
+            }
+
+            UpdateStatus();
+        }
+
+        private void UpdateStatus()
+        {
+            var installed = GetAllModFiles(fdGTA.SelectedPath);
+            var stashed = GetAllModFiles(stashPath);
+            11
+            if (installed.Count > 0 )
+            {
+                lblStatus.Text = "Installed";
+                lblStatus.ForeColor = Color.FromArgb(22, 148, 56);
+            }
+            else
+            {
+                lblStatus.Text = "Uninstalled";
+                lblStatus.ForeColor = Color.FromArgb(176, 25, 40);
+            }
+        }
+        
+    }
+}
